75fbe997d631b05ee2ffb1d6c0d71767dce8a598
Simin Afshar <145557376+SiminAfshar@users.noreply.github.com>
2025-12-04 01:33:29 +0330

    Update getGirlImage.jsx

--- src/Utils/getGirlImage.jsx
+++ src/Utils/getGirlImage.jsx
@@ -2,20 +2,20 @@ export default function getGirlImage(selected) {
     const { necklace, earrings, bracelet, ring, neckleswith } = selected;
   
     if (bracelet)
-      return "/Images/model/modeling 1.jpg";
+      return "/images/model/modeling 1.jpg";
   
     if (ring)
-      return "/Images/model/modeling 2.jpg";
+      return "/images/model/modeling 2.jpg";
     if (neckleswith)
-        return "/Images/model/modeling 4.jpg";
+        return "/images/model/modeling 4.jpg";
     
     if (necklace)
-      return "/Images/model/modeling 3.jpg";
+      return "/images/model/modeling 3.jpg";
   
     
     if (earrings)
-      return "/Images/model/modeling 5.jpg";
+      return "/images/model/modeling 5.jpg";
   
-    return "/Images/model/modeling 6.jpg";
+    return "/images/model/modeling 6.jpg";
   }
   
